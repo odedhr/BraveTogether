e49509e6d4a01151f13208c8eb27282827fac1c1
odedhr <odedhr@gmail.com>
2021-03-30 14:08:17 +0300

    Varela

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import AppBar from './components/AppBar/AppBar';
 import Main from './components/Main/Main';
+import GlobalStyle from './globalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AppBar></AppBar>
       <Main></Main>
     </div>
