909ad5a87dfef458b521abedcdbca286521081d9
odedhr <odedhr@gmail.com>
2021-03-30 13:50:45 +0300

    globalstyles

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppBar from './components/AppBar/AppBar';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <AppBar></AppBar>
+      <Main></Main>
     </div>
   );
 }
---
+++ Client/src/globalSyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+`;
+ 
+export default GlobalStyle;
