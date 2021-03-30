fbb083e9db1c0d432fb1d8656782295d488caec6
odedhr <odedhr@gmail.com>
2021-03-30 13:14:29 +0300

    Main

---
+++ Client/src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface IMainProps {
+}
+
+export default function Main (props: IMainProps) {
+  return (
+    <div>
+      <div>
+          </div>
+    </div>
+  );
+}
