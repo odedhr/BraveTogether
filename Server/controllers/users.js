53b2a0031470044c0c3c78132ec2cc2beb76285e
Boban Sugareski <boban.s@slicelife.com>
2021-03-31 15:03:46 +0200

    Add api sync on user create

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -1,4 +1,5 @@
 const { User } = require('../models');
+const axios = require('axios');
 
 module.exports = {
   fetchAll: async (req, res) => {
@@ -39,22 +40,38 @@ module.exports = {
 
     res.send(content);
   },
-  create: (req, res) => {
-    User.create({
-      ...req.body,
-      image: req.file.path
-    }).then(user => {
-        res.status(201).send({
-          error: false,
-          message: 'User successfully added',
-          user
-        });
-      })
-      .catch(err => {
-        res.send({
-          error: true,
-          message: err.message
-        });
+  create: async (req, res) => {
+    // TODO: Sync with the API
+    try {
+      var apiUser = await axios.post('http://127.0.0.1:5000/user', {
+        email: req.body.email,
+        password: req.body.password,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        cellphone: req.body.cellphone
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
       });
+
+      var user = await User.create({
+        ...req.body,
+        user_id: apiUser.data.id,
+        image: req.file ? req.file.path : null
+      });
+    
+      res.send({
+        error: false,
+        message: 'User successfully added',
+        api_user: apiUser ? apiUser.data : {},
+        user: user
+      });
+    } catch (err) {
+      res.send({
+        error: true,
+        message: err.response.data.message
+      });
+    }
   }
 };
