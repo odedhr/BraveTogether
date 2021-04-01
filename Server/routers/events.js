46ec52dfd066ee9e7701625134f0dfa4d126fbee
Boban Sugareski <boban.s@slicelife.com>
2021-04-01 04:28:14 +0200

    Fetching API data & merging event data to fetchOne & fetchAll

--- Server/routers/events.js
+++ Server/routers/events.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/events');
 
-router.get('/', controller.fetchAll)
+router.post('/', controller.fetchAll)
       .post('/:id', controller.fetchOne)
       .post('/', controller.create);
 
